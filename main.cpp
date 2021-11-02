e14624f19d5ada4793b30663c2ffdba644e8f1bb
Ioannis Mandralis <ioannima@ethz.ch>
2021-11-02 01:11:41 -0700

    building main simulation class

--- main.cpp
+++ main.cpp
@@ -10,6 +10,131 @@
 using Eigen::MatrixXd;
 using Eigen::Vector2d;
 
+class Simulation {
+public:
+    // Physics
+    double Re;
+    // Spatial discretization
+    double Lx;
+    double Ly;
+    int Nx;
+    int Ny;
+    int Nxs;
+    int Nys;
+    double dx;
+    double dy;
+    // Temporal discretization
+    double T;
+    int Nt;
+    double dt;
+    // Velocity field and pressure field in row major order on staggered grid
+    // Keep matrices dynamic for now...don't know how to deal with fixed size matrices in classes
+    MatrixXd u;
+    MatrixXd v;
+    MatrixXd p;
+    // Interpolated quantities
+    MatrixXd uq;
+    MatrixXd vq;
+    // Derivatives
+    MatrixXd lapU;
+    MatrixXd lapV;
+    MatrixXd duvdx;
+    MatrixXd duvdy;
+    MatrixXd duvdxC;
+    MatrixXd duvdyC;
+    MatrixXd duudx;
+    MatrixXd dvvdy;
+
+    // Constructor
+    Simulation(double Re_, double Lx_, double Ly_, double T_, int Nx_, int Ny_, int Nt_)
+      : Re(Re_), Lx(Lx_),Ly(Ly_),T(T_), Nx(Nx_),Ny(Ny_),Nt(Nt_),dx(Lx_/Nx_),dy(Ly/Ny),dt(T/Nt),Nxs(Nx_-1),Nys(Ny_-1),
+        u((Nx_-1)*(Ny_-1),1), v((Nx_-1)*(Ny_-1),1), p((Nx_-1)*(Ny_-1),1),
+        uq((Nx-1)*(Ny-1),1), vq((Nx-1)*(Ny-1),1),lapU((Nx-1)*(Ny-1),1), lapV((Nx-1)*(Ny-1),1),
+        duvdx((Nx_-1)*(Ny_-1),1), duvdy((Nx_-1)*(Ny_-1),1), duvdxC((Nx_-1)*(Ny_-1),1), duvdyC((Nx_-1)*(Ny_-1),1),
+        duudx((Nx_-1)*(Ny_-1),1), dvvdy((Nx_-1)*(Ny_-1),1){}
+
+    void interpUVFaceToCorner() {
+        // Function that linearly interpolates the grid velocities from the left and bottom face to
+        // the lower left corner
+        int Nq = Nxs * Nys;
+        for (int i=0;i<Nq;i++) {
+            int idxU = i+Nxs;
+            if (idxU > Nq-1) {idxU = idxU-Nq;}
+            int idxV = i-1;
+            if (i%Nxs==0) {idxV = idxV+Nxs;}
+            uq(i) = 0.5 * (u(i) + u(idxU));
+            vq(i) = 0.5 * (v(i) + v(idxV));
+        }
+    }
+
+    void interpCrossTermCornerToFace() {
+        // Function that linearly interpolates the cross derivative from the lower left corner to the left
+        // and bottom face
+        // duvdy should be on the left face
+        // duvdx should be on the bottom face
+        int Nq = Nxs * Nys;
+        for (int i=0;i<Nq;i++) {
+            int idxU = i+Nxs;
+            if (idxU > Nq-1) {idxU = idxU-Nq;}
+            int idxV = i-1;
+            if (i%Nxs==0) {idxV = idxV+Nxs;}
+            uq(i) = 0.5 * (u(i) + u(idxU));
+            vq(i) = 0.5 * (v(i) + v(idxV));
+        }
+    }
+
+    void computeLaplacian() {
+        // Function that computes Laplacian on grid faces
+        int Nq = Nxs * Nys;
+        for (int i=0;i<Nq;i++) {
+            int iX_ = i-1;
+            int iXX = i+1;
+            if(i%Nxs){iX_=iX_+Nxs;}
+            if((i+1)%Nxs) {iXX=iXX-Nxs;}
+            int iY_ = i-Nxs;
+            int iYY = i+Nxs;
+            if(iY_<0) {iY_=iY_+Nq;}
+            if(iYY>Nq-1) {iYY=iYY-Nq;}
+            lapU(i) = 1/(dx*dx) * (u(iXX)-2*u(i)+u(iX_)) +  1/(dy*dy) * (u(iYY)-2*u(i)+u(iY_));
+            lapV(i) = 1/(dx*dx) * (v(iXX)-2*v(i)+v(iX_)) +  1/(dy*dy) * (v(iYY)-2*v(i)+v(iY_));
+        }
+    }
+
+    void computeSquareTerm() {
+        // Function that duudx and dvvdy on grid faces
+        int Nq = Nxs * Nys;
+        for (int i=0;i<Nq;i++) {
+            int iX_ = i-1;
+            int iXX = i+1;
+            if(i%Nxs){iX_=iX_+Nxs;}
+            if((i+1)%Nxs) {iXX=iXX-Nxs;}
+            int iY_ = i-Nxs;
+            int iYY = i+Nxs;
+            if(iY_<0) {iY_=iY_+Nq;}
+            if(iYY>Nq-1) {iYY=iYY-Nq;}
+            duudx(i) = 1/(2*dx) * (u(iXX)*u(iXX)-u(iX_)*u(iX_));
+            dvvdy(i) = 1/(2*dy) * (v(iYY)*v(iYY)-v(iY_)*v(iY_));
+        }
+    }
+
+    void computeCrossTermAtCorners() {
+        // Function that computes duvdx and duvdy on grid corners
+        int Nq = Nxs * Nys;
+        for (int i=0;i<Nq;i++) {
+            int iX_ = i-1;
+            int iXX = i+1;
+            if(i%Nxs){iX_=iX_+Nxs;}
+            if((i+1)%Nxs) {iXX=iXX-Nxs;}
+            int iY_ = i-Nxs;
+            int iYY = i+Nxs;
+            if(iY_<0) {iY_=iY_+Nq;}
+            if(iYY>Nq-1) {iYY=iYY-Nq;}
+            duvdxC(i) = 1/(2*dx) * (uq(iXX)*vq(iXX)-uq(iX_)*vq(iX_));
+            duvdyC(i) = 1/(2*dy) * (uq(iYY)*vq(iYY)-uq(iY_)*vq(iY_));
+        }
+    }
+};
+
 // Spring mass system RHS function
 Vector2d compute_rhs_springmass(const Vector2d &u) {
     double m = 1.0; // careful...hardcoded
@@ -115,36 +240,51 @@ int main() {
     saveData("N.csv",N_mat);
     return 0;
 }
-
-//#include <nr3.h>
-//#include <stepper.h>
-//#include <odeint.h>
-//#include <stepperdopr5.h>
-
-//// RHS function (let's use a functor instead of a bare function)
-//struct rhs_van {
-//    double mu;
-//    rhs_van(double mu_) : mu(mu_) {}
-//    void operator() (const double t, const Vector2d &u, Vector2d &dudt) {
-//        dudt[0] = u[1];
-//        dudt[1] = (mu*(1.0-u[0]*u[0])*u[1]-u[0]);
+//
+//MatrixXd interpolateU() {
+//    // Function that linearly interpolates the grid
+//    // Matrix containing interpolated values at the corners of the grid (green points)
+//    int Nq = Nx*Ny;
+//    MatrixXd uq(Nq,1);
+//    int Nu = Nx*(Ny-1);
+//    // First interpolate all interior points and deal with boundary points later.
+//    // Reminder: we are interpolating to bottom left corner.
+//    for (int i=0;i<Nq;i++) {
+//        // First Nx points on the upper boundary
+//        if (i<Nx) {
+//            //Periodic boundary conditions
+//            uq(i,1) = 0.5*(u(i,1) + u(Nu-Nx+i));
+//        }
+//        // Last Nx points on the lower boundary
+//        if (i>=Nq-Nx) {
+//            uq(i,1) = 0.5*(u(Nu-Nx+i,1) + u(i-Nu,1));
+//        }
+//        // For all other points
+//        uq(i,1) = 0.5 * (u(i-Nx,1) + u(i,1));
 //    }
-//};
-
-//int main() {
-//    const int nvar=2;
-//    const double atol=1.0e-3, rtol=atol, h1=0.01, hmin=0.0, x1=0.0, x2=2.0;
-//    Vector2d ystart(nvar);
-//    ystart[0]=2.0;
-//    ystart[1]=0.0;
-//    Output out(20);
-//    rhs_van d(1.0e-3);
-//    Odeint<StepperDopr5<rhs_van> > ode(ystart,x1,x2,atol,rtol,h1,hmin,out,d);
-//    ode.integrate();
+//    return uq;
+//}
 //
-//    // Print the output
-//    for (int i=0;i<out.count;i++)
-//        cout << out.xsave[i] << " " << out.ysave[0][i] << " " << out.ysave[1][i] << endl;
-//
-//    return 0;
+//MatrixXd interpolateV() {
+//    // Function that linearly interpolates the grid
+//    // Matrix containing interpolated values at the corners of the grid (green points)
+//    int Nq = Nx*Ny;
+//    MatrixXd vq(Nq,1);
+//    int Nv = (Nx-1)*Ny;
+//    // First interpolate all interior points and deal with boundary points later.
+//    // Reminder: we are interpolating to bottom left corner.
+//    for (int i=0;i<Nq;i++) {
+//        // First Nx points on the upper boundary
+//        if (i<Nx) {
+//            //Periodic boundary conditions
+//            vq(i,1) = 0.5*(u(i,1) + u(Nv-Nx+i));
+//        }
+//        // Last Nx points on the lower boundary
+//        if (i>=Nq-Nx) {
+//            vq(i,1) = 0.5*(u(Nv-Nx+i,1) + u(i-Nv,1));
+//        }
+//        // For all other points
+//        vq(i,1) = 0.5 * (u(i-Nx,1) + u(i,1));
+//    }
+//    return vq;
 //}
