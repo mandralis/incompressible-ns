d812af865d5366ad6ccd8e97d4f5d629a3f82da1
Yanni Mandralis <imandralis@Yannis-MacBook-Pro.local>
2021-11-06 12:41:59 -0700

    added preliminary poisson solver and projection step

--- io-tools/eigen-io.h
+++ io-tools/eigen-io.h
@@ -47,7 +47,7 @@ MatrixXd openData(string fileToOpen)
 
     // here we convet the vector variable into the matrix and return the resulting object,
     // note that matrixEntries.data() is the pointer to the first memory location at which the entries of the vector matrixEntries are stored;
-    return Map<Matrix<double, Dynamic, Dynamic, RowMajor>>(matrixEntries.data(), matrixRowNumber, matrixEntries.size() / matrixRowNumber);
+    return Map<Matrix<double, Dynamic, Dynamic, RowMajor> >(matrixEntries.data(), matrixRowNumber, matrixEntries.size() / matrixRowNumber);
 
 }
 
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <Eigen/Dense>
 #include <vector>
 #include <eigen-io.h>
+#include <fftw3.h>
 
 #define _USE_MATH_DEFINES
 
@@ -14,13 +15,14 @@ class Simulation {
 public:
     // Physics
     double Re;
-    // Spatial discretization
+        // Spatial discretization
     double Lx;
     double Ly;
     int Nx;
     int Ny;
     int Nxs;
     int Nys;
+    int Nq;
     double dx;
     double dy;
     // Temporal discretization
@@ -32,31 +34,112 @@ public:
     MatrixXd u;
     MatrixXd v;
     MatrixXd p;
-    // Interpolated quantities
+    // Interpolated quantities (at cell corners)
     MatrixXd uq;
     MatrixXd vq;
-    // Derivatives
+    // Interpolated quantities (at cell centers)
+    MatrixXd um;
+    MatrixXd vm;
+    // Spatial Derivatives
     MatrixXd lapU;
     MatrixXd lapV;
+    MatrixXd convU;
+    MatrixXd convV;
+    MatrixXd div;
     MatrixXd duvdx;
     MatrixXd duvdy;
     MatrixXd duvdxC;
     MatrixXd duvdyC;
     MatrixXd duudx;
     MatrixXd dvvdy;
+    MatrixXd dpdx;
+    MatrixXd dpdy;
+    // Time Derivatives
+    MatrixXd dudt;
+    MatrixXd dvdt;
+    // FFTW setup
+    fftw_complex *in, *out;
+    fftw_plan plan;
 
     // Constructor
     Simulation(double Re_, double Lx_, double Ly_, double T_, int Nx_, int Ny_, int Nt_)
       : Re(Re_), Lx(Lx_),Ly(Ly_),T(T_), Nx(Nx_),Ny(Ny_),Nt(Nt_),dx(Lx_/Nx_),dy(Ly/Ny),dt(T/Nt),Nxs(Nx_-1),Nys(Ny_-1),
-        u((Nx_-1)*(Ny_-1),1), v((Nx_-1)*(Ny_-1),1), p((Nx_-1)*(Ny_-1),1),
-        uq((Nx-1)*(Ny-1),1), vq((Nx-1)*(Ny-1),1),lapU((Nx-1)*(Ny-1),1), lapV((Nx-1)*(Ny-1),1),
-        duvdx((Nx_-1)*(Ny_-1),1), duvdy((Nx_-1)*(Ny_-1),1), duvdxC((Nx_-1)*(Ny_-1),1), duvdyC((Nx_-1)*(Ny_-1),1),
-        duudx((Nx_-1)*(Ny_-1),1), dvvdy((Nx_-1)*(Ny_-1),1){}
+        Nq(Nxs*Nys), u(Nq,1), v(Nq,1), p(Nq,1), uq(Nq,1), vq(Nq,1),lapU(Nq,1), lapV(Nq,1),
+        duvdx(Nq,1), duvdy(Nq,1), duvdxC(Nq,1), duvdyC(Nq,1), duudx(Nq,1), dvvdy(Nq,1),
+        convU(Nq,1), convV(Nq,1), dudt(Nq,1), dvdt(Nq,1), um(Nq,1), vm(Nq,1), div(Nq,1),
+        dpdx(Nq,1), dpdy(Nq,1){
+        // Don't do in place transforms...requires extra considerations
+        in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * Nq);
+        out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * Nq);
+        plan = fftw_plan_dft_2d(Nxs, Nys, in, out, FFTW_FORWARD, FFTW_MEASURE);
+    }
+
+    void advance() {
+        VectorXd k1u(Nq,1);
+        VectorXd k2u(Nq,1);
+        VectorXd k3u(Nq,1);
+        VectorXd k1v(Nq,1);
+        VectorXd k2v(Nq,1);
+        VectorXd k3v(Nq,1);
+        // Step 1
+        computeRHS();
+        k1u = dt * dudt;
+        k1v = dt * dvdt;
+        u = u + 0.5 * k1u;
+        v = v + 0.5 * k1v;
+        computeProjectionStep();
+        // Step 2
+        computeRHS();
+        k2u = dt * dudt;
+        k2v = dt * dvdt;
+        u = u - 1.5 * k1u + 2 * k2u;
+        v = v - 1.5 * k1v + 2 * k2v;
+        computeProjectionStep();
+        // Step 3
+        computeRHS();
+        k3u = dt * dudt;
+        k3v = dt * dvdt;
+        u = u + 7.0/6.0 * k1u -  4.0/3.0 * k2u + 1.0/6.0 * k3u;
+        v = v + 7.0/6.0 * k1v -  4.0/3.0 * k2v + 1.0/6.0 * k3v;
+        computeProjectionStep();
+    }
+
+    void computeProjectionStep() {
+        computeDivergence();
+        solvePoissonEq();
+        applyFractionalStep();
+    }
+
+    void solvePoissonEq() {
+        // Write values to in. We want to fourier transform the RHS of the divergence equation, so the divergence should be written to 
+
+        fftw_execute(plan); /* repeat as needed */
+        fftw_destroy_plan(plan);
+        fftw_free(in); fftw_free(out);
+    }
+
+    void applyFractionalStep() {
+        // Function that computes the divergence free velocity field given the pressure gradients
+        u = u - dt * dpdx;
+        v = v - dt * dpdy;
+    }
+
+    void interpUVCenter() {
+        // Function that linearly interpolates the grid velocities from the left and bottom face to
+        // the center
+        for (int i=0;i<Nq;i++) {
+            int iXX = i+1;
+            if((i+1)%Nxs) {iXX=iXX-Nxs;}
+            int iY_ = i-Nxs;
+            if(iY_<0) {iY_=iY_+Nq;}
+            um(i) = 0.5 * (u(i) + u(iXX));
+            vm(i) = 0.5 * (v(i) + v(iY_));
+        }
+    }
 
     void interpUVFaceToCorner() {
         // Function that linearly interpolates the grid velocities from the left and bottom face to
         // the lower left corner
-        int Nq = Nxs * Nys;
         for (int i=0;i<Nq;i++) {
             int idxU = i+Nxs;
             if (idxU > Nq-1) {idxU = idxU-Nq;}
@@ -72,20 +155,43 @@ public:
         // and bottom face
         // duvdy should be on the left face
         // duvdx should be on the bottom face
-        int Nq = Nxs * Nys;
         for (int i=0;i<Nq;i++) {
-            int idxU = i+Nxs;
-            if (idxU > Nq-1) {idxU = idxU-Nq;}
-            int idxV = i-1;
-            if (i%Nxs==0) {idxV = idxV+Nxs;}
-            uq(i) = 0.5 * (u(i) + u(idxU));
-            vq(i) = 0.5 * (v(i) + v(idxV));
+            int iXX = i+1;
+            if((i+1)%Nxs) {iXX=iXX-Nxs;}
+            int iY_ = i-Nxs;
+            if(iY_<0) {iY_=iY_+Nq;}
+            duvdx(i) = 0.5*(duvdxC(i) + duvdxC(iXX));
+            duvdy(i) = 0.5*(duvdyC(i) + duvdyC(iY_));
         }
+    }
+
+    void computeRHS() {
+        computeLaplacian();
+        computeConvectiveTerm();
+        dudt = -convU + lapU/Re;
+        dvdt = -convV + lapV/Re;
+    }
+
+    void computeDivergence() {
+        // Function that computes divergence in cell centers
+        // since this is where the pressure is advanced
+        interpUVCenter();
+        for (int i=0;i<Nq;i++) {
+            int iX_ = i-1;
+            int iXX = i+1;
+            if(i%Nxs){iX_=iX_+Nxs;}
+            if((i+1)%Nxs) {iXX=iXX-Nxs;}
+            int iY_ = i-Nxs;
+            int iYY = i+Nxs;
+            if(iY_<0) {iY_=iY_+Nq;}
+            if(iYY>Nq-1) {iYY=iYY-Nq;}
+            div(i) = 1/(2*dx) * (u(iXX)-u(iX_)) + 1/(2*dy) * (v(iYY)-u(iY_));
+        }
+
     }
 
     void computeLaplacian() {
         // Function that computes Laplacian on grid faces
-        int Nq = Nxs * Nys;
         for (int i=0;i<Nq;i++) {
             int iX_ = i-1;
             int iXX = i+1;
@@ -100,9 +206,17 @@ public:
         }
     }
 
+    void computeConvectiveTerm() {
+        interpUVFaceToCorner();
+        computeCrossTermAtCorners();
+        interpCrossTermCornerToFace();
+        computeSquareTerm();
+        convU = duudx + duvdy;
+        convV = duvdx + dvvdy;
+    }
+
     void computeSquareTerm() {
-        // Function that duudx and dvvdy on grid faces
-        int Nq = Nxs * Nys;
+        // Function that computes duudx and dvvdy on grid faces
         for (int i=0;i<Nq;i++) {
             int iX_ = i-1;
             int iXX = i+1;
@@ -119,7 +233,6 @@ public:
 
     void computeCrossTermAtCorners() {
         // Function that computes duvdx and duvdy on grid corners
-        int Nq = Nxs * Nys;
         for (int i=0;i<Nq;i++) {
             int iX_ = i-1;
             int iXX = i+1;
